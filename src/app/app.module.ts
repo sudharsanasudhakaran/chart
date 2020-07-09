34baf8a8f20054a3ec650555452f94895eae6595
Sudhakaran <SUDHARSANA.SUDHAKARAN@Chubb.com>
2020-07-09 18:46:26 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BarchartComponent } from './barchart/barchart.component';
@@ -34,6 +34,9 @@ import { ChartsModule} from 'ng2-charts';
     ChartsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
